c721605652b5d53c885113ed8aa51cb45784eed5
Ramsey Hauanio <hauanio@usc.edu>
2025-09-13 01:13:22 +0000

    all done with part 2

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -9,5 +9,78 @@
 
 int main(int argc, char* argv[])
 {
+    // Scenario 1: removing item from empty list to front of empty list
+        ULListStr list1;
+
+        std::cout << "scenario 1: remove from empty list \n";
+        if (list1.empty()) {
+            std::cout << "list empty \n";
+        }
+        
+        if (list1.size() == 0) {
+            std::cout << "size 0 \n";
+        }
+
+        // try removing elements
+        list1.pop_back();
+        std::cout << "removed nothing \n";
+        list1.pop_front();
+        std::cout << "removed nothing \n \n";
+
+    // Scenario 1: removing item from empty list to an empty list
+        ULListStr list2;
+
+        std::cout << "scenario 2: add to an empty list \n";
+
+        // add element to front
+        std::cout << "add element: ";
+        list2.push_front("1");
+        
+        std::cout << list2.front() << "\n";
+        list2.pop_back();
+        std::cout << "removed element \n";
+
+        // add element to back
+        std::cout << "add element: ";
+        list2.push_back("2");
+        std::cout << "\n";
+        list2.pop_front();
+        std::cout << "removed element \n \n";
+
+
+    // Scenario 3: adding item to a non-empty list
+        ULListStr list3;
+
+        std::cout << "scenario 3: nonempty list with room on both ends \n";
+        if (!list3.empty()) {
+            std::cout << "list not empty \n";
+        }
+        
+        std::cout << "list size: " << list3.size() << "\n \n";
+
+    // Scenario 4:
+    // try adding elements
+    ULListStr list4;
+
+    std::cout << " Scenario 4: nonempty list with no room on both ends \n";
+
+    // Push elements so list is (3, 1, 2)
+    list4.push_back("1");
+    list4.push_back("2");
+    list4.push_front("3");  
+
+    std::cout << "list size after pushes: " << list4.size() << "\n"; // 3
+    std::cout << "front element: " << list4.front() << "\n"; //  "3"
+    std::cout << "back element: " << list4.back() << "\n";   // "2"
+
+
+    // Pop from both ends
+    std::cout << "remove element: ";
+    list4.pop_front();
+    std::cout << "front element: " << list4.front() << "\n"; //  "3"
+    list4.pop_back();  // remove
+
+    std::cout << "List size after pops: " << list4.size();
+    std::cout << "Front element (only Z left): " << list4.front();
 
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -24,7 +24,169 @@ size_t ULListStr::size() const
   return size_;
 }
 
-// WRITE YOUR CODE HERE
+// push_back: add value to end of list
+  void ULListStr::push_back(const std::string& val) {
+    // case 1: list empty
+      if (empty()) { 
+        // add a first node and update pointers
+        Item* newNode = new Item();
+        tail_ = newNode;
+        head_ = tail_;
+      }
+    
+    // case 2: list is full
+      else if (tail_->last == ARRSIZE) {
+        // add a new node and update pointers
+        Item* newNode = new Item();
+        newNode->prev = tail_;
+        tail_->next = newNode;
+        tail_ = newNode; 
+      }
+
+    // case 3: list has room
+      // add new element to list
+      tail_->val[tail_->last] = val;
+      tail_->last++;
+
+      size_++; // update size
+    return;
+  }
+
+// push_front: add element to front of list
+  void ULListStr::push_front(const std::string& val) {
+    // case 1: list empty
+    if (empty()) {
+      // allocate first node and update pointers
+      Item* newNode = new Item();
+      head_ = newNode;
+      tail_ = head_;
+
+      // start filling in from the last slot
+      head_->first = ARRSIZE; 
+      head_->last  = ARRSIZE;
+    }
+
+    // case 2: list is full at the front
+    else if (head_->first == 0) {
+      Item* newNode = new Item();
+      newNode->next = head_;
+      head_->prev = newNode;
+      head_ = newNode;
+
+      // start filling in from the last slot
+      head_->first = ARRSIZE; 
+      head_->last  = ARRSIZE;
+    }
+
+    // case 3: room at front
+      // add value to the list
+      head_->first--;
+      head_->val[head_->first] = val;
+
+      size_++; // update size
+    return;
+  }
+  
+
+// pop_back: remove element from end of list
+void ULListStr::pop_back() {
+  // case 1: empty list - nothing to remove
+  if (empty()){
+    return; 
+  }
+
+  // case 2: nonempty list 
+    // remove the last element from tail
+    tail_->last--;
+    size_--;
+  
+  // if there was only 1 node in list
+  if (tail_->first == tail_->last) { 
+    //  separate old empty tail node
+    Item* old = tail_;
+    tail_ = tail_->prev;
+
+    // update head or tail if necessary
+    if (tail_){ 
+    tail_->next = nullptr;
+    }
+    else {
+      head_ = nullptr;
+    }    
+    delete old; // delete old tail node
+  }  
+
+}
+// pop_front: remove element from front of list
+void ULListStr::pop_front() {
+  // case 1: empty list - nothing to remove
+  if (empty()){
+    return;
+  }
+
+  // case 2: nonempty list
+    // remove the 1st element from the head 
+    head_->first++;  
+    size_--;         
+
+  // if this head node is now empty
+  if (head_->first == head_->last) {
+    // separate old head node
+    Item* old = head_;       
+    head_ = head_->next;     
+
+    // update head or tail if necessary
+    if (head_) {
+      head_->prev = nullptr; 
+    } else {
+      tail_ = nullptr;      
+    }
+
+    delete old; // delete old head node
+  }
+}
+
+
+// front return value at the front of the list
+std::string const & ULListStr::front() const {
+  size_t firstIdx = head_->first;
+  return head_->val[firstIdx];
+}
+
+// back: return value at end of the list
+std::string const & ULListStr::back() const {
+  size_t lastIdx = tail_->last - 1;
+  return tail_->val[lastIdx];
+}
+
+
+// getValAtLoc: return ptr to the i-th element in the array 
+std::string* ULListStr::getValAtLoc(size_t loc) const {
+  // case 1: non-existent location provided
+  if (loc >= size_) {
+    return NULL;
+  }
+
+  // case 2: valid location
+  size_t idx = loc;
+  Item* temp = head_;
+
+  while (temp != nullptr) {
+
+    // number of elements in this node
+    size_t cnt = temp->last - temp->first;
+
+    if (idx < cnt) {
+      // the element is inside this node
+      return &temp->val[temp->first + idx];
+    }
+
+    // skip this node and reduce the index accordingly
+    idx -= cnt;
+    temp = temp->next;
+  }
+  return NULL;
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
@@ -63,3 +225,4 @@ void ULListStr::clear()
   tail_ = NULL;
   size_ = 0;
 }
+
